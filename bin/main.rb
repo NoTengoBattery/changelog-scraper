c5aa08c29815344c7d9d7bb2c409a0f0be195eb1
Oever González <notengobattery@gmail.com>
2021-01-24 19:15:20 -0600

    main: refactor main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -5,41 +5,41 @@ require_relative '../lib/provider'
 require_relative '../lib/printer'
 
 Blessings.output_stream = $stderr
+MORE_HELP = 'For more information about the usage of this script, run it with the -h flag.'
 
 begin
   options = GitHubLogManOptparser.new.parse(ARGV)
 rescue OptionParser::ParseError => e
-  MyUtils.exit_on_exception(
-    e, 'For more information about the usage of this script, run it with the -h flag.',
-    PARSER_ECODE
-  )
+  MyUtils.exit_on_exception(e, MORE_HELP, PARSER_ECODE)
 end
 MyUtils.verbose = options.verbose
 
 MyUtils.note('The program has started. Please wait while the request is completed...')
 MyUtils.note(nil)
-MyUtils.note('This process may take a while for large changelogs.')
+MyUtils.note('This process may take a while, specially for large changelogs.')
 MyUtils.note('Enable the verbose mode to see the proccess as it runs.')
-MyUtils.note('For more information about the usage of this script, run it with the -h flag.')
+MyUtils.note('The script may fail due to excessive request to the provider, be careful to not hit the limit.')
+MyUtils.note(nil)
+MyUtils.note(MORE_HELP)
 begin
   provider = ProviderFactory.new.build(options.url)
 rescue NoProviderError => e
   MyUtils.exit_on_exception(
-    e, 'Run this scirpt with the verbose option to see all the available providers. For more help use the -h flag.',
+    e, "Run this scirpt with the verbose option to see all the available providers.\n#{MORE_HELP}",
     PROVIDER_ECODE
   )
 rescue HTTP::ConnectionError => e
   MyUtils.exit_on_exception(
-    e, 'Provide a valid URL and ensure a stable internet connection.',
+    e, "Provide a valid URL and ensure a stable internet connection.\n#{MORE_HELP}",
     HTTP_ECODE
   )
 rescue ScraperError => e
   MyUtils.exit_on_exception(
-    e, "Please report this issue to the mantainer. Enable the verbose mode and send a copy of the log.\n\t#{MANTAINER}",
+    e, "Please report this issue to the mantainer. Enable the verbose mode and send a copy of the log.\n#{MORE_HELP}",
     SCRAPER_ECODE
   )
 end
 MyUtils.note('All information was correctly retrieved from the internet')
 
-printer = PrinterFactory.new.build(options.printer)
+printer = PrinterFactory.build(options.printer)
 printer.print_changelog(provider.changelog)
