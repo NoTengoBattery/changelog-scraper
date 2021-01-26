dfaa7fa10d3d1f264807743b21b80d49384c541b
Oever González <notengobattery@gmail.com>
2021-01-26 09:14:37 -0600

    rspec: use a boarder exception for HTTP
    This will help to avoid problems when a site changes it's return error code or the server changes (like going down, redirecting or showing a new page).

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'StrictHTTP' do
   let(:timeout) { 'https://gi.com' }
   let(:good) { 'https://github.com' }
   it 'rejects valid URLs that return HTTP error codes' do
-    expect { StrictHTTP.strict_get(valid_but_error, 3) }.to raise_error(HTTP::ConnectionError)
+    expect { StrictHTTP.strict_get(valid_but_error, 3) }.to raise_error(HTTP::Error)
   end
   it 'rejects valid URLs that lead to DNS NXDOMAIN' do
-    expect { StrictHTTP.strict_get(valid_but_nx, 3) }.to raise_error(HTTP::ConnectionError)
+    expect { StrictHTTP.strict_get(valid_but_nx, 3) }.to raise_error(HTTP::Error)
   end
   it 'rejects valid URLs that lead to server errors' do
-    expect { StrictHTTP.strict_get(timeout, 3) }.to raise_error(HTTP::ConnectionError)
+    expect { StrictHTTP.strict_get(timeout, 3) }.to raise_error(HTTP::Error)
   end
   it 'accepts valid HTTP URLs that return HTTP success codes' do
     expect { StrictHTTP.strict_get(good, 3) }.not_to raise_error
